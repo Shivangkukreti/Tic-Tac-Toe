9b6609b013ec49b6b765c7a8861a4cc77eaaab59
Shivangkukreti <shivangkukretiwork@gmail.com>
2025-01-14 00:26:43 +0530

    resolved

--- code.js
+++ code.js
@@ -1,60 +1,4 @@
-<<<<<<< HEAD
-let text =document.querySelector("#text")
-let all=document.querySelectorAll(".all")
-let count=0
-let x=[]
-let o=[]
-let win=['123','369','789','147','258','159','357'] //hello
 
-
-all.forEach((ele)=>{
-    ele.addEventListener("click",()=>{
-        
-        if (ele.textContent=='') {
-         count++
-         if (count%2==0) {
-            ele.textContent="O"
-            o.push(ele.getAttribute("id"))
-            check()
-            ele.style.pointerEvents="none"  
-         } 
-         else{
-            ele.textContent="X"
-            x.push(ele.getAttribute("id"))
-            ele.style.pointerEvents="none"
-            check()
-         }  
-        }
-    })
-})
-
-
-function check() {
-    for (const ele of win) {
-        if (ele.split('').every((char)=>x.includes(char))) {
-            text.textContent="X WINS!!"
-            all.forEach((any)=>{any.style.pointerEvents="none"})
-        }
-        if(ele.split('').every((char)=>o.includes(char))){
-            text.textContent="O WINS!!"
-            all.forEach((any)=>{any.style.pointerEvents="none"})
-        }
-    }
-
-}
-
-text.addEventListener("click",()=>{
-        x=[]
-        o=[]
-        text.textContent="START"
-        count=0
-        text.style.transform="scale(1.1)"
-        all.forEach((any)=>{
-            any.style.pointerEvents="all"
-            any.textContent=""
-        })
-    }
-=======
 let text =document.querySelector("#text")
 let all=document.querySelectorAll(".all")
 let count=0
@@ -110,5 +54,5 @@ text.addEventListener("click",()=>{
             any.textContent=""
         })
     }
->>>>>>> 29e1aaf (done)
+
 )
